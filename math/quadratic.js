defba61443b3fb2cbf05c02fd9f2011eff926b0e
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2020-05-21 19:45:57 -0400

    Update quadratic.js

--- math/quadratic.js
+++ math/quadratic.js
@@ -1,1 +1,14 @@
-
+//function
+function quadratic() {
+//declare variables a, b, c
+var a = document.getElementById("a").value;
+var b = document.getElementById("b").value;
+var c = document.getElementById("c").value;
+  
+//work
+var ans1 = -b + (sqrt(math.pow(b, 2) - 4 * a * c) / 2 * a)
+return ans1;
+//var ans2 is the same except theres a minus instead of plus
+var ans2 = -b - (sqrt(math.pow(b, 2) - 4 * a * c) / 2 * a)
+return ans2;
+}
