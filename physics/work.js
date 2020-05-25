72d94eb642ceb950f18ed4d8c0aaa0be91670d66
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2020-05-25 19:45:17 -0400

    Update work.js

--- physics/work.js
+++ physics/work.js
@@ -1,6 +1,10 @@
 //finds work done in joules 
-
+fucntion work() {
+//define vars
 var force = document.getElementById("force");
 var d = document.getElementById("d");
 var q = document.setElementById("theta")
-
+//work
+var work = force * d * Math.cos(q);
+return work;
+}
