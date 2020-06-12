c180aede54f8f21688d850319d7fe2ea4e4a3cea
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2020-06-12 17:22:36 -0400

    Create slope.js

---
+++ math/slope.js
@@ -0,0 +1,8 @@
+var x1 = document.getElementById("x1").value;
+var y1 = document.getElementById("y1").value;
+var x2 = document.getElementById("x2").value;
+var y2 = document.getElementById("y2").value;
+
+function findSlope() {
+
+}
